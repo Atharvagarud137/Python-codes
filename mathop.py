4045e0752ded81ae2150c15680200e2721ccbf82
Atharvagarud137 <92423386+Atharvagarud137@users.noreply.github.com>
2022-04-11 14:59:38 +0530

    Update mathop.py

--- mathop.py
+++ mathop.py
@@ -1,18 +1,21 @@
 print("Enter a number:")
 num=int(input())
-sqrt=num**0.5
+if num<0:
+    print("Square root of the number cannot be calculated.")
+else:
+    sqrt=num**0.5
+    print("Square root of the number is: ",sqrt)
 sq=num**2
 cube=num**3
 print("Square of the number is: ",sq)
-print("Square root of the number is: ",sqrt)
 print("Cube of the number is: ",cube)
-if num > 1:
-    for i in range(2, int((num/2)+1)):
-        if (num%i) == 0:
-            print(num,"is not a prime number.")
-            break
+if num < 0:
+    print("Prime number is not defined for negative numbers.")
+else:
+    if num > 1:
+        for i in range(2, int((num/2)+1)):
+            if (num%i) == 0:
+                print(num,"is not a prime number.")
+                break
     else:
         print(num,"is a prime number.")
-        
-else:
-    print(num,"is not a prime number.")
